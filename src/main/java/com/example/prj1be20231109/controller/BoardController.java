c3d5c1c38ca3afbbdb60d741da85fbaed6c282a3
dasom <zzzoo42@naver.com>
2023-11-09 14:08:51 +0900

    응답 ok, internalServerError status 작성

--- src/main/java/com/example/prj1be20231109/controller/BoardController.java
+++ src/main/java/com/example/prj1be20231109/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.example.prj1be20231109.controller;
 import com.example.prj1be20231109.domain.Board;
 import com.example.prj1be20231109.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +19,12 @@ public class BoardController {
     private final BoardService service;
 
     @PostMapping("add")
-    public void add(@RequestBody Board board){
+    public ResponseEntity add(@RequestBody Board board){
 
-        service.save(board);
+        if ( service.save(board)) {
+            return ResponseEntity.ok().build();
+        } else  {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
