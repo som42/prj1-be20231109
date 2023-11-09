a7b900956a4b3a551cde7b2a1e469d65d5d167f3
dasom <zzzoo42@naver.com>
2023-11-09 12:30:01 +0900

    새 게시글 쓰기 controller, service, mapper, dto, table table 만든 쿼리 CREATE TABLE board (     id INT PRIMARY KEY AUTO_INCREMENT,     title VARCHAR(200) NOT NULL ,     content VARCHAR(1000) NOT NULL ,     writer VARCHAR(100) NOT NULL ,     inserted DATETIME NOT NULL  DEFAULT NOW() );

---
+++ src/main/java/com/example/prj1be20231109/service/BoardService.java
@@ -0,0 +1,17 @@
+package com.example.prj1be20231109.service;
+
+import com.example.prj1be20231109.domain.Board;
+import com.example.prj1be20231109.mapper.BoardMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardMapper mapper;
+
+    public void save(Board board) {
+        mapper.insert(board);
+    }
+}
