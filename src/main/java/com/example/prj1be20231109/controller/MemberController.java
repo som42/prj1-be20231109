82ee02452a3471498092cfcebefc8331c744f9bd
som42 <zzzoo42@naver.com>
2023-11-12 21:21:49 +0900

    회원 가입 기능 완성 table CREATE TABLE member (     id VARCHAR(100) primary key,     password VARCHAR(100) not null ,     email VARCHAR(100) not null ,     inserted DATETIME DEFAULT NOW() );

--- src/main/java/com/example/prj1be20231109/controller/MemberController.java
+++ src/main/java/com/example/prj1be20231109/controller/MemberController.java
@@ -1,20 +1,22 @@
 package com.example.prj1be20231109.controller;
 
 import com.example.prj1be20231109.domain.Member;
+import com.example.prj1be20231109.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/member")
 public class MemberController {
+    private final MemberService service;
+
     @PostMapping("signup")
     public void signup(@RequestBody Member member){
-        System.out.println("member = " + member);
+        service.add(member);
+
     }
 }
