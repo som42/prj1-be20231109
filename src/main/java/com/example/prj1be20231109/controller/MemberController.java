b12e7d20043712c65655ca2104be455e73434184
som42 <zzzoo42@naver.com>
2023-11-13 09:39:32 +0900

    회원 정보 보기 작성중

--- src/main/java/com/example/prj1be20231109/controller/MemberController.java
+++ src/main/java/com/example/prj1be20231109/controller/MemberController.java
@@ -48,4 +48,9 @@ public class MemberController {
     public List<Member> list(){
         return service.list();
     }
+
+    @GetMapping
+    public void view(String id){
+        System.out.println("id = " + id);
+    }
 }
