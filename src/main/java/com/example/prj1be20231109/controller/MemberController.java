18bb26b1c2cd113ea884c7cde37b477c4bc03532
som42 <zzzoo42@naver.com>
2023-11-14 11:47:53 +0900

    회원 수정시 권한 검증

--- src/main/java/com/example/prj1be20231109/controller/MemberController.java
+++ src/main/java/com/example/prj1be20231109/controller/MemberController.java
@@ -95,7 +95,15 @@ public class MemberController {
     }
 
     @PutMapping("edit")
-    public ResponseEntity edit(@RequestBody Member member) {
+    public ResponseEntity edit(@RequestBody Member member,
+                               @SessionAttribute(value = "login", required = false)Member login) {
+        if (login == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); //401
+        }
+        if (!service.hasAccess(member.getId(), login)){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build(); //403
+        }
+
         // TODO: 로그인 했는지? 자기정보인지?
 
         if (service.update(member)) {
