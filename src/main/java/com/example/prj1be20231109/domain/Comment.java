02a7e8f4927d2efe0db31f53e6b55beeb3364b7a
som42 <zzzoo42@naver.com>
2023-11-19 14:24:49 +0900

    Board.ageAgo 작성 Comment.getAgo 작성

--- src/main/java/com/example/prj1be20231109/domain/Comment.java
+++ src/main/java/com/example/prj1be20231109/domain/Comment.java
@@ -1,5 +1,6 @@
 package com.example.prj1be20231109.domain;
 
+import com.example.prj1be20231109.util.AppUtil;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -12,4 +13,8 @@ public class Comment {
     private String memberNickName;
     private String comment;
     private LocalDateTime inserted;
+
+    public String getAgo(){
+        return AppUtil.getAgo(inserted, LocalDateTime.now());
+    }
 }
