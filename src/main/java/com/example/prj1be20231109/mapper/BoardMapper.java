b2b5f5c5207eaffd3f9888d6e9773506296b074d
som42 <zzzoo42@naver.com>
2023-11-19 13:29:21 +0900

    게시물 목록에 좋아요 수 표시

--- src/main/java/com/example/prj1be20231109/mapper/BoardMapper.java
+++ src/main/java/com/example/prj1be20231109/mapper/BoardMapper.java
@@ -16,16 +16,18 @@ public interface BoardMapper {
 
 
     @Select("""
-       SELECT b.id,
-              b.title,
-              b.writer,
-              m.nickName,
-              b.inserted,
-          COUNT(c.id) countComment
-        FROM board b JOIN member m ON b.writer = m. id
-                LEFT JOIN comment c ON b.id = c.boardId
-        GROUP BY b.id
-        ORDER BY b.id DESC;  
+           SELECT b.id,
+           b.title,
+           b.writer,
+           m.nickName,
+           b.inserted,
+           COUNT(DISTINCT c.id) countComment,
+           COUNT(DISTINCT l.id) countLike
+    FROM board b JOIN member m ON b.writer = m. id
+                 LEFT JOIN comment c ON b.id = c.boardId
+                 LEFT JOIN boardLike l ON b.id = l.boardId
+    GROUP BY b.id
+    ORDER BY b.id DESC;
             """)
     List<Board> selectAll();
 
