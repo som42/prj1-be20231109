45f557e0209b9af31c45a0654c123cae9e7592f8
som42 <zzzoo42@naver.com>
2023-11-21 09:35:44 +0900

    첨부된 이미지 파일 수 응답

--- src/main/java/com/example/prj1be20231109/mapper/BoardMapper.java
+++ src/main/java/com/example/prj1be20231109/mapper/BoardMapper.java
@@ -17,22 +17,23 @@ public interface BoardMapper {
 
 
     @Select("""
-           SELECT b.id,
-           b.title,
-           b.writer,
-           m.nickName,
-           b.inserted,
-           COUNT(DISTINCT c.id) countComment,
-           COUNT(DISTINCT l.id) countLike
-           FROM board b JOIN member m ON b.writer = m. id
-                 LEFT JOIN comment c ON b.id = c.boardId
-                 LEFT JOIN boardLike l ON b.id = l.boardId
-                 
-           WHERE b.content LIKE #{keyword}
-           OR b.title LIKE #{keyword}
-          GROUP BY b.id
-          ORDER BY b.id DESC
-          LIMIT #{from}, 10
+            SELECT b.id,
+               b.title,
+               b.writer,
+               m.nickName,
+               b.inserted,
+               COUNT(DISTINCT c.id) countComment,
+               COUNT(DISTINCT l.id) countLike,
+               COUNT(DISTINCT f.id) countFile
+            FROM board b JOIN member m ON b.writer = m. id
+                     LEFT JOIN comment c ON b.id = c.boardId
+                     LEFT JOIN boardLike l ON b.id = l.boardId
+                     LEFT JOIN boardFile f ON b.id = f.boardId
+            WHERE b.content LIKE '%%'
+                OR b.title LIKE '%%'
+            GROUP BY b.id
+            ORDER BY b.id DESC
+            LIMIT 0,10;
           """)
     List<Board> selectAll(Integer from, String keyword);
 
