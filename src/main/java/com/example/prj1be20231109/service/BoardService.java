0562714c655baf6b4c9ebe73a0de1189231036ff
som42 <zzzoo42@naver.com>
2023-11-21 10:29:38 +0900

    첨부파일 있는 게시물 삭제 (s3 bucket object 도)

--- src/main/java/com/example/prj1be20231109/service/BoardService.java
+++ src/main/java/com/example/prj1be20231109/service/BoardService.java
@@ -14,6 +14,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.Delete;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
@@ -141,10 +143,31 @@ public class BoardService {
         //좋아요 레코드 지우기
         likeMapper.deleteByBoardId(id);
 
-        // 이미지 있는 게시물 지우기
-        fileMapper.deleteByBoardId(id);
+        deleteFile(id);
+
+
         return mapper.deleteById(id) == 1;
 
+    }
+
+    private void deleteFile(Integer id){
+        // 파일명 조회
+        List<BoardFile> boardFiles = fileMapper.selectNamesByBoardId(id);
+
+        // s3 bucket objects 지우기
+        for ( BoardFile file : boardFiles){
+            String key = "prj1/" + id + "/" + file.getName();
+
+            DeleteObjectRequest objectRequest= DeleteObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(key)
+                    .build();
+
+            s3.deleteObject(objectRequest);
+        }
+
+        // 이미지 있는 게시물 지우기
+        fileMapper.deleteByBoardId(id);
     }
 
     public boolean update(Board board) {
